e609b3804f1276f106afe0d5b920fa1ad7ab710c
duy07t1k23cht <bbbshurikengerbbb@gmail.com>
2020-06-01 22:31:15 +0700

    Add Base History

---
+++ app/src/main/java/com/example/cameratranslator/model/HistoryItem.java
@@ -0,0 +1,35 @@
+package com.example.cameratranslator.model;
+
+/**
+ * Created by Duy M. Nguyen on 6/1/2020.
+ */
+class HistoryItem {
+
+    private String imagePath;
+    private String dateModified;
+    private BoundingPoly boundingPoly;
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public BoundingPoly getBoundingPoly() {
+        return boundingPoly;
+    }
+
+    public void setBoundingPoly(BoundingPoly boundingPoly) {
+        this.boundingPoly = boundingPoly;
+    }
+}
